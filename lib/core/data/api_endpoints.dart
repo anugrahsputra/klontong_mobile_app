bb803167a78e6678da1b9dc3efab45c4f1aedcb7
anugrahsputra <anugrahsputra@gmail.com>
2025-07-14 20:58:44 +0700

    Docs: Update product API endpoint key in ApiEndpoints
    This commit updates the `_productBaseUrl` in `ApiEndpoints.dart`.
    
    The API key for the `crudcrud.com` service has been changed from a specific key to `CRUD_API_KEY`. This is likely a placeholder indicating that a valid API key needs to be provided, possibly through environment variables or a configuration file, before the product-related API endpoints can be used.

--- lib/core/data/api_endpoints.dart
+++ lib/core/data/api_endpoints.dart
@@ -1,7 +1,7 @@
 abstract class ApiEndpoints {
   static String get _baseUrl => "https://reqres.in";
   static String get _productBaseUrl =>
-      "https://crudcrud.com/api/eac9572521e348f6b81bf1385967d49c";
+      "https://crudcrud.com/api/CRUD_API_KEY";
 
   static String get login => "$_baseUrl/api/login";
   static String get register => "$_baseUrl/api/register";
