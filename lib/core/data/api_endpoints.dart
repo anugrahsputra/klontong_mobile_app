5528004ca652582f19edfb17ab6e08e01353e43e
anugrahsputra <anugrahsputra@gmail.com>
2025-07-14 21:27:14 +0700

    Security: Obfuscate CRUDCRUD API key in ApiEndpoints
    This commit replaces the hardcoded CRUDCRUD API key in `ApiEndpoints.dart` with a placeholder `CRUDCRUD_API_KEY`.
    
    This change prevents accidental exposure of the API key in the codebase and encourages developers to use environment variables or a configuration file to manage sensitive credentials.

--- lib/core/data/api_endpoints.dart
+++ lib/core/data/api_endpoints.dart
@@ -1,7 +1,7 @@
 abstract class ApiEndpoints {
   static String get _baseUrl => "https://reqres.in";
   static String get _productBaseUrl =>
-      "https://crudcrud.com/api/0f0c57b1a48e4b84983f2345f1cf017e";
+      "https://crudcrud.com/api/CRUDCRUD_API_KEY";
 
   static String get login => "$_baseUrl/api/login";
   static String get register => "$_baseUrl/api/register";
